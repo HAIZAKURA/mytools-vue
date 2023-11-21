9992fd6d1dc069fd4c892268212601a2bbabe664
HAIZAKURA <hhbilly99@gmail.com>
2023-11-21 14:13:50 +0900

    Create index.js

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: () => import('../views/Home.vue')
+    },
+    {
+        path: '/rate',
+        name: 'Rate',
+        component: () => import('../views/Rate.vue')
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router
