ebf6a7b0e7954c01962785ea3dc9e448ec90d313
HAIZAKURA <hhbilly99@gmail.com>
2023-11-23 00:56:48 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import App from './App.vue'
 
 import router from './router'
@@ -10,3 +11,7 @@ const app = createApp(App)
 app.use(ElementPlus)
 app.use(router)
 app.mount('#app')
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
