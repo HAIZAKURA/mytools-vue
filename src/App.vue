54008c6c32e4319fada7421582ceeed992dae89a
HAIZAKURA <hhbilly99@gmail.com>
2025-05-18 19:30:45 +0900

    add: IPv6 Support Badge

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,20 @@
-<script setup>
-</script>
-
 <template>
   <el-container class="lab">
 
     <el-header class="lab-header">
-
       <el-text tag="b" size="large" class="lab-title" @click="goBackHome">HAIZAKURA Lab</el-text>
-
       <el-divider />
-
     </el-header>
 
     <el-main class="lab-main">
-
       <router-view />
-
     </el-main>
+
+    <el-footer class="fixed-bottom-center">
+      <a href="https://ipw.cn/ipv6webcheck/?site=lab.nya.run" title="本站支持IPv6访问" target="_blank">
+        <img class="ipv6-badge" alt="本站支持IPv6访问" src="https://static.ipw.cn/icon/ipv6-certified-lite-s4.svg">
+      </a>
+    </el-footer>
 
   </el-container>
 </template>
@@ -26,7 +24,7 @@ export default {
   methods: {
       goBackHome() {
           this.$router.push({ name: 'Home' });
-      }
+      },
   }
 };
 </script>
@@ -42,4 +40,17 @@ export default {
 
 .lab-header
   margin-top 1em
+
+.fixed-bottom-center
+  position fixed
+  bottom 0
+  left 50%
+  transform translate(-50%, 0)
+  text-align center
+  width 100%
+  z-index 999
+
+.ipv6-badge
+  display inline-block
+  vertical-align middle
 </style>
